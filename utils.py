56e8dc5f631acd9818f390c47002538c7357006a
nikitakosatka <nikitakosatka@yandex.ru>
2022-04-05 22:43:13 +0300

    Creating Genesis

--- utils.py
+++ utils.py
@@ -8,3 +8,11 @@ def get_hash(filename):
     file = open(blockchain_dir + filename, mode='rb').read()
 
     return hashlib.md5(file).hexdigest()
+
+
+def create_genesis():
+    if not os.path.isdir(blockchain_dir):
+        os.mkdir(blockchain_dir)
+
+        with open(blockchain_dir + '1', mode='w') as file:
+            file.write("Genesis block\n")
